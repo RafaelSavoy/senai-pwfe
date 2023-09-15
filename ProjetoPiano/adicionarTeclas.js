65f94b7fcf034d278e9f72ef47e99988e85409ca
RafaelSavoy <rafaelsavoy1@outlook.com>
2023-09-14 21:57:10 -0300

    adding-files

---
+++ ProjetoPiano/adicionarTeclas.js
@@ -0,0 +1,18 @@
+const teclas = document.getElementsByClassName('tecla')
+
+function tocarNota(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const tecla = document.querySelector(`div[data-key="${e.keyCode}"]`)
+
+    tecla.style.boxShadow = 'inset 0 0 10px #000'
+    audio.currentTime = 0;
+    audio.play();
+}
+
+window.addEventListener('keydown', tocarNota)
+
+window.addEventListener('keyup', (e) => {
+    const tecla = document.querySelector(`div[data-key="${e.keyCode}"]`)
+    tecla.style.backgroundColor = 'white'
+    tecla.style.boxShadow = ''
+})
